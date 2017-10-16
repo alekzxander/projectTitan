b8d5972ba60b050516ee9cb7afb00b0a03847b7e
Alexandre Callens <alekzxand@gmail.com>
2017-10-16 09:11:52 +0400

    images

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ let app = express();
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res)=>{
+    res.render('carte.ejs');
+})
 
 app.listen(2501);
