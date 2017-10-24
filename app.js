06961ff03807b31d48609ef9cf2cece379a8cb98
LauGR <laurent.gregoire974@gmail.com>
2017-10-24 09:56:13 +0400

    add contact

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-
-// set up 
-// get all the tools we need
+// set up get all the tools we need
 
 let express = require('express');
 let ejs = require('ejs');
 let app = express();
-var port     = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
-
-
-
-var morgan       = require('morgan');
+var flash = require('connect-flash');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var ConnectRoles = require('connect-roles');
-
+const nodemailer = require("nodemailer");
 var configDB = require('./config/database.js');
 
-// configuration 
-mongoose.connect(configDB.url, { useMongoClient: true }); // connection database
+// configuration
+mongoose.connect(configDB.url, {useMongoClient: true}); // connection database
 mongoose.Promise = global.Promise;
 
 require('./config/passport')(passport); // pass passport for configuration
@@ -30,7 +25,7 @@ require('./config/passport')(passport); // pass passport for configuration
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
@@ -44,11 +39,12 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-// routes 
+// routes
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs')
 
-app.set('view engine','ejs')
 
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
